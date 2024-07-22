f2b5b98d74069c2921c8b8aa7a58923cc9ff33ab
Hyeokjin Yun <yhjin1096@gamil.com>
2024-07-22 11:55:16 +0900

    Rename the files

--- src/stereo_pr/src/main.cpp
+++ src/stereo_pr/src/main.cpp
@@ -1,61 +1,98 @@
 #include "stereo_pr/stereo_pr.hpp"
 
+enum Mode
+{
+    save,
+    query
+};
+
 int main(int argc, char **argv)
 {
-    std::string left_path = "/home/cona/Downloads/dataset/data_odometry_gray/dataset/sequences/00/image_0/";
-    std::string right_path = "/home/cona/Downloads/dataset/data_odometry_gray/dataset/sequences/00/image_1/";
-    std::vector<cv::Mat> gt_poses;
-    int num_images = 0;
-    CountImages(num_images, left_path);
-    readGTPose("/home/cona/Downloads/dataset/data_odometry_gray/data_odometry_poses/dataset/poses/00.txt", gt_poses);
-    
+    std::string gray_path = "/home/cona/Downloads/dataset/data_odometry_gray/dataset/sequences/00/";
+    std::string color_path = "/home/cona/Downloads/dataset/data_odometry_color/dataset/sequences/00/";
+    int num_images = utils::CountImages(gray_path + "image_0/");
+    std::cout << "num_images: " << num_images << std::endl;
 
-    Tracker tracker;
-    Visualizer visualizer;
-    int gap = 1;
-    
+    FeatureTracker ft;
+    PlaceRecognition pr;
+    Visualizer viz;
+    Mode mode = Mode::query;
+
+    std::vector<Node> nodes;
     cv::Mat traj = cv::Mat::zeros(cv::Size(1000,1000), CV_8UC3);
-    cv::Mat prev_pose = cv::Mat::eye(4, 4, CV_64F);
-    
-    for(int i = 0; i < num_images-gap; i++)
+    int wait_sec = 1;
+
+    for(int i = 0; i < num_images; i++)
     {
-        Node refer, query;
+        // Node node(i, color_path, true);
+        Node node(i, gray_path, false);
+        ft.featureExtract(node.left_cam);
+        ft.featureExtract(node.right_cam);
 
-        refer.left_cam.loadImage(left_path + cv::format("%06d.png", i));
-        refer.right_cam.loadImage(right_path + cv::format("%06d.png", i));
-        query.left_cam.loadImage(left_path + cv::format("%06d.png", i+gap));
-        query.right_cam.loadImage(right_path + cv::format("%06d.png", i+gap));
-        
-        /*feature extract*/
-        // tracker.extractKeypointsFAST(refer.left_cam); //FAST
-        tracker.extractKeypointsORB(refer.left_cam); //ORB
-        tracker.extractKeypointsORB(refer.right_cam); //ORB
-        
-        /*tracking*/
-        /*trackKLTAndDescriptor: refer 좌우측에서 feature 추출*/
-        tracker.trackKLTAndDescriptor(refer, query);
-        
-        /*get 3d point*/
-        tracker.calc3DPoints(refer);
+        node.left_cam.changeStructure();
+        node.right_cam.changeStructure();
 
-        /*calculate pose*/
-        tracker.calcPose(refer, query);
-        
-        // std::cout << "frame" << i << " -> frame" << i+1 << std::endl; 
-        // std::cout << query.left_cam.cam_to_world_pose << std::endl;
-        // std::cout << "" << std::endl;
-        
-        prev_pose = prev_pose * query.left_cam.cam_to_world_pose;
-        
-        // cv::Mat viz_track, viz_extract;
-        // visualizer.visualizeExtractAll(refer, query, viz_extract);
-        // visualizer.visualizeTracking(refer, query, viz_track);
-        // visualizer.visualizeRelative3D(myWindow, refer, query, gt_poses[i], gt_poses[i+1], i);
-        visualizer.visualizeTrajectory(prev_pose, traj);
-        char key = cv::waitKey(1);
-        if(key == 'q')
+        if(i != 0)
+        {
+            // get stereo matching
+            nodes[i-1].stereo_matches = ft.getDescMatching(nodes[i-1].left_cam.descriptors, nodes[i-1].right_cam.descriptors);
+            viz.vizMatches(nodes[i-1], true);
+            nodes[i-1].removeMatchingOutlier();
+
+            // tracking
+            Node tmp_node = ft.trackImage(nodes[i-1], node);
+            viz.vizTracking(nodes[i-1], tmp_node, true);
+
+            // get 3d point
+            nodes[i-1].points3D = ft.calc3DPoints(nodes[i-1].left_cam, nodes[i-1].right_cam);
+
+            // pose estimation
+            ft.calcPose(nodes[i-1], tmp_node);
+            node.cam_to_world_pose = tmp_node.cam_to_world_pose.clone();
+            node.world_to_cam_pose = tmp_node.world_to_cam_pose.clone();
+            node.rot_rodrigues = tmp_node.rot_rodrigues.clone();
+            node.translation = tmp_node.translation.clone();
+            viz.vizTrajectory(node.cam_to_world_pose, traj);
+        }
+
+        // place recognition
+        cv::Mat pr_traj = traj.clone();
+        if(mode == Mode::save)
+            pr.stackFeatures(node.left_cam.row_descriptor);
+        else if(mode == Mode::query)
+        {
+            if(i == 0)
+            {
+                pr.loadDatabase("ORBdb.yml.gz");
+            }
+            else
+            {
+                // vo // node[i]
+                std::vector<DBoW2::Result> pr_list = pr.queryDatabase(node.index, node.left_cam.row_descriptor, 10);
+                if(pr_list.size())
+                {
+                    viz.vizPRTrajectory(nodes, node, pr_list, pr_traj);
+                    wait_sec = 0;
+                }
+                else
+                    wait_sec = 1;
+            }
+        }
+
+        nodes.push_back(node);
+        char key = cv::waitKey(wait_sec);
+        if(key == 27)
             break;
     }
+    
+    Timer timer;
+    timer.tic();
+    if(mode == Mode::save) // database 생성
+    {
+        // pr.VocCreation();
+        pr.createDatabase("ORBvoc.txt");
+    }
+    timer.toc();
     
     return 0;
 }
